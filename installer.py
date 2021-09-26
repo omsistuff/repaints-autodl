9ec1f07365af592621ecc2f1219ca6f2321638ce
Aryqs-Ipsum <mrfeyurikenpro@gmail.com>
2021-09-26 14:28:54 +0200

    update installer

--- installer.py
+++ installer.py
@@ -1,7 +1,152 @@
 import os
+import requests
+from tkinter import *
+from tkinter.ttk import *
+import zipfile
+import time
+import winreg
+import ctypes, sys
 
 if __name__ == "__main__":
-    # TODO: prendre le reg et le exe de main.py sur le cdn
-    # TODO: les placer dans les bons dossiers respectifs
-    # TODO: empaqueter dans un installer.exe
-    os.startfile('osfrinstall.reg')
+    program_path = r"C:\Program Files (x86)\Steam\steamapps\common\OMSI 2\.omsistuff"
+    autodl_path = os.path.join(program_path, "autodl")
+    md5file = os.path.join(program_path, "autodl.md5")
+    tmp_loc = os.path.join(program_path, "autodl.tmp.zip")
+    build_url = "https://firebasestorage.googleapis.com/v0/b/objects-omsistuff.appspot.com/o/programs%2Fautodl%2Fbuild.zip"
+    download_link = build_url + "?alt=media"
+    metadata = requests.get(build_url).json()
+
+    def resourcePath(relative_path):
+        if hasattr(sys, '_MEIPASS'):
+            return os.path.join(sys._MEIPASS, relative_path)
+        return os.path.join(os.path.abspath("."), relative_path)
+
+    path = winreg.HKEY_CLASSES_ROOT
+    autodl_path_reg = r"autodlomsistuff\\shell\\open\\command"
+    def read_reg(k=''):
+        try:
+            key = winreg.OpenKeyEx(path, autodl_path_reg)
+            value = winreg.QueryValueEx(key,k)
+            if key:
+                winreg.CloseKey(key)
+            return value[0]
+        except Exception as e:
+            print(e)
+        return None
+
+    def save_reg(v=0, p=""):
+        try:
+            key = winreg.CreateKey(path, p)
+            winreg.SetValueEx(key, '', 0, winreg.REG_SZ, str(v))
+            if key:
+                winreg.CloseKey(key)
+            return True
+        except Exception as e:
+            raise e
+
+    # not working actually
+    # return file not found error in admin mode
+    def save_or_fail_reg():
+        if read_reg() is None:
+            try:
+                save_reg(p=r"autodlomsistuff")
+                save_reg(p=r"autodlomsistuff\\shell")
+                save_reg(p=r"autodlomsistuff\\shell\\open")
+                save_reg(p=autodl_path ,v='c:\Program Files (x86)\Steam\steamapps\common\OMSI 2\.omsistuff\autodl\main.exe "%1"')
+            except:
+                print('restart in admin mode')
+                ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+
+    reg_attemps = 0
+    def open_reg_file():
+        global reg_attemps
+        reg_attemps+=1
+        if reg_attemps < 3:
+            try:
+                os.startfile(os.path.join(autodl_path, 'url_protocol.reg'))
+            except:
+                message = "Veuillez accepter les droits a cette étape.\nCeci permet au logiciel de s'ouvrir depuis le navigateur.\n(nouvel essai dans 5s)"
+                currentTask.set(message)
+                window.update()
+                time.sleep(5)
+                open_reg_file()
+
+    def main():
+        # read lasest hash file
+        if not os.path.exists(md5file):
+            f = open(md5file, 'w')
+            f.close()
+
+        hash_file = open(md5file, "r+")
+        hash_from_file = hash_file.readline()
+
+        if hash_from_file != metadata["md5Hash"]:
+            currentTask.set("Téléchargement de la nouvelle version")
+            window.update()
+            with open(tmp_loc, "wb") as f:
+                response = requests.get(download_link, stream=True)
+                total_length = response.headers.get('content-length')
+
+                if total_length is None: # no content length header
+                    f.write(response.content)
+                else:
+                    dl = 0
+                    total_length = int(total_length)
+                    for data in response.iter_content(chunk_size=1024):
+                        dl += len(data)
+                        f.write(data)
+                        bar['value'] = int(100 * dl / total_length)
+                        window.update()
+
+            # extarct zip
+            currentTask.set("Extraction de l'archive")
+            window.update()
+            with zipfile.ZipFile(tmp_loc, 'r') as zf:
+                # Get a list of all archived file names from the zip
+                listOfFileNames = zf.namelist()
+                # Iterate over the file names
+                for fileName in listOfFileNames:
+                    # try to extract
+                    try:
+                        zf.extract(fileName, autodl_path)
+                    except:
+                        pass
+            # remove tmp archive
+            try:
+                os.remove(tmp_loc)
+            except:
+                pass
+
+            # open registry file
+            if read_reg() is None:
+                open_reg_file()
+
+            # write new hash to file
+            hash_file.seek(0)
+            hash_file.write(metadata["md5Hash"])
+            hash_file.truncate()
+
+        else:
+            currentTask.set("Programme deja à jour")
+            window.update()
+            time.sleep(2)
+
+        # close window
+        window.destroy()
+
+    # interface
+    window = Tk()
+    window.title("omsistuff autodl - mise a jour")
+    window.geometry("320x100+0+0")
+    window.resizable(False, False)
+    window.iconbitmap(resourcePath("icon.ico"))
+
+    currentTask = StringVar()
+
+    bar = Progressbar(window,orient=HORIZONTAL,length=300)
+    bar.pack(pady=15)
+    currentTaskLabel = Label(window,textvariable=currentTask).pack()
+    # button = Button(window,text="Installer",command=main).pack()
+
+    window.after_idle(main)
+    window.mainloop()
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
+Build program
 ```bat
 python setup.py build
+```
+
+Build installer
+```bat
+pyinstaller installer.py -F -c -n "autodl_setup.exe" --icon=icon.ico --add-data "icon.ico;."
 ```
